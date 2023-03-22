532526255dc8a7235a1bb38b15f952fa1f4917c8
Jan Verrept <janverrept@me.com>
2023-03-22 22:50:34 +0100

    finding the right structure

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "ClibModbus",
-            targets: ["ClibModbus"]),
+            targets: ["libModbus"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,7 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .systemLibrary(
-            name: "ClibModbus",
+            name: "libModbus",
             pkgConfig: "libmodbus",
             providers: [.brew(["libmodbus"])]
         )
